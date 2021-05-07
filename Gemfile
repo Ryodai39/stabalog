9f4e53d4741cadc6744fb78550956431895414ef
EC2 Default User <ec2-user@ip-172-31-32-81.us-east-2.compute.internal>
2021-05-07 10:13:01 +0000

    新規ユーザのプロフィール実装

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ group :development, :test do
   gem "rspec-rails", "~> 4.0.0.beta2"
   gem "rails-controller-testing"
   gem "rubocop-airbnb"
-  gem "factory_bot_rails"
+  gem "factory_bot_rails", "~> 4.10.0"
 end
 
 group :development do
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -15,5 +15,27 @@ RSpec.describe "Users", type: :system do
         expect(page).to have_title full_title('ユーザー登録')
       end
     end
+    context "ユーザー登録処理" do
+      it "有効なユーザーでユーザー登録を行うとユーザー登録成功のフラッシュが表示されること" do
+        fill_in "ユーザー名", with: "Example User"
+        fill_in "メールアドレス", with: "user@example.com"
+        fill_in "パスワード", with: "password"
+        fill_in "パスワード(確認)", with: "password"
+        click_button "登録する"
+        expect(page).to have_content "スタバログへようこそ！"
+      end
+   
+      it "無効なユーザーでユーザー登録を行うとユーザー登録失敗のフラッシュが表示されること" do
+        fill_in "ユーザー名", with: ""
+        fill_in "メールアドレス", with: "user@example.com"
+        fill_in "パスワード", with: "password"
+        fill_in "パスワード(確認)", with: "pass"
+        click_button "登録する"
+        expect(page).to have_content "ユーザー名を入力してください"
+        expect(page).to have_content "パスワード(確認)とパスワードの入力が一致しません"
+      end
+    end
   end
+
+
 end
